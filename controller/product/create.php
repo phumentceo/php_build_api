342000a47a17d67c217ebe4379933ef244248e22
phumentceo <roungchanrith8@gmail.com>
2024-12-07 15:51:49 +0700

    PHP API with Image

--- controller/product/create.php
+++ controller/product/create.php
@@ -7,15 +7,47 @@ header("Access-Control-Allow-Methods:POST");
 
 $method = $_SERVER['REQUEST_METHOD'];  
 
-if ($method === 'POST') {  
+if ($method === 'POST') { 
 
 
-    $data = json_decode(file_get_contents("php://input"),true);
+    $contentType = $_SERVER["CONTENT_TYPE"] ?? '';
 
-    $name = $data['name'];
-    $price = $data['price'];
-    $qty   = $data['quantity'];
-    
+    $name = '';
+    $price = '';
+    $qty   = '';
+
+    if (strpos($contentType, 'application/json') !== false) {
+        // If Content-Type is JSON, decode the JSON payload
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        $name = $data['name'] ?? '';
+        $price = $data['price'] ?? '';
+        $qty = $data['quantity'] ?? '';
+
+    }else{
+
+        // If Content-Type is form data
+        $name = $_POST['name'] ?? '';
+        $price = $_POST['price'] ?? '';
+        $qty = $_POST['quantity'] ?? '';
+
+        $image = $_FILES['image']['name'];
+        //move to the directory
+
+        $imageName = rand(0,999999999) .'.'. pathinfo($image,PATHINFO_EXTENSION);
+
+
+        //move directory
+        $target_file = "../../public/$imageName";
+
+        move_uploaded_file($_FILES['image']['tmp_name'],$target_file);
+
+
+    }
+
+    //store to db
+    $imageStore = "http://localhost:3000/public/$imageName";
+
 
     //create rules validation with request
     $errors = [];  
@@ -31,13 +63,13 @@ if ($method === 'POST') {
     
     if (!isset($qty) ||  $qty < 0 || intval($qty) != $qty) {  
         $errors['quantity'] = "quantity must be a non-negative integer.";  
-    }  
+    } 
 
       
     if (!empty($errors)) {  
         http_response_code(400);  
         echo json_encode([  
-            "status" => 400,  
+            "status" => 400,
             "message" => "Validation failed.",  
             "errors" => $errors  
         ]);  
@@ -50,7 +82,7 @@ if ($method === 'POST') {
 
 
     //store to database
-    $sql = "INSERT INTO `products`(`name`,`price`,`quantity`) VALUES('$name','$price','$qty')";
+    $sql = "INSERT INTO `products`(`name`,`price`,`quantity`,`image`) VALUES('$name','$price','$qty','$imageStore')";
     $result = mysqli_query($conn,$sql);
     
     //current insert with id
